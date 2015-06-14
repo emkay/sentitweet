8f70e780c7e8e3d47ef3aa9a86c1f2cc5425f1ae
Michael Matuzak <mmatuzak@gmail.com>
2015-06-13 20:57:51 -0700

    returning obj for gather

--- gather.js
+++ gather.js
@@ -8,29 +8,31 @@ const levelWs = require('level-ws')
 const tracking = require('./tracking')
 
 var db = levelWs(level('./sentiment-db'))
-var ws = db.createWriteStream()
-
-var client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-})
-
-ws.on('error', function (err) {
-  console.error('Error:', err)
-})
-ws.on('close', function () {
-  console.log('Stream closed')
-})
 
 function gather () {
+  var ws = db.createWriteStream()
+
+  var client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  })
+
+  ws.on('error', function (err) {
+    console.error('Error:', err)
+  })
+  ws.on('close', function () {
+    console.log('Stream closed')
+  })
+
   client.stream('statuses/filter', {track: tracking.join(',')}, function (s) {
     s.on('data', function (tweet) {
       var text = tweet && tweet.lang === 'en' && tweet.text
       var results = text && sentiment(text)
 
       if (results) {
+        console.log(results)
         ws.write({ key: cuid(), value: JSON.stringify(results)})
       }
     })
@@ -41,4 +43,11 @@ function gather () {
   })
 }
 
-module.exports = gather
+function valueStream () {
+  return db.createValueStream()
+}
+
+module.exports = {
+  gather: gather,
+  valueStream: valueStream
+}
